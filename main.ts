4d4716cfe4e592ef20c10124485393e92edac7a4
cammclain <cammclain@protonmail.com>
2024-10-23 16:04:40 -0400

    adding capabilties to select a word with `Alt-w` & `d` to delete selected text

--- main.ts
+++ main.ts
@@ -1,134 +1,107 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
+// Plugin settings interface and defaults
 interface MyPluginSettings {
-	mySetting: string;
+    mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+    mySetting: 'default',
+};
+
+export default class HelixKeybindingPlugin extends Plugin {
+    settings: MyPluginSettings;
+
+    async onload() {
+        await this.loadSettings();
+
+        // Register the commands for Helix-like actions
+        this.addCommand({
+            id: 'select-word',
+            name: 'Select word under cursor',
+            editorCallback: this.selectWordUnderCursor,
+        });
+
+        this.addCommand({
+            id: 'delete-selection',
+            name: 'Delete selected text',
+            editorCallback: this.deleteSelection,
+        });
+
+        // Register keybindings to trigger the above commands
+        this.registerDomEvent(document, 'keydown', (evt: KeyboardEvent) => {
+            if (evt.altKey && evt.key === 'w') {
+                this.app.commands.executeCommandById('helix-keybindings-plugin.select-word');
+            }
+            if (evt.key === 'd') {
+                this.app.commands.executeCommandById('helix-keybindings-plugin.delete-selection');
+            }
+        });
+
+        // Add a settings tab for user configurations
+        this.addSettingTab(new HelixSettingTab(this.app, this));
+    }
+
+    onunload() {
+        console.log('Unloading Helix Keybindings Plugin...');
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings);
+    }
+
+    // Function to select a word under the cursor
+    selectWordUnderCursor(editor: Editor) {
+        const cursor = editor.getCursor();
+        const word = editor.getWordAt(cursor);
+        if (word) {
+            editor.setSelection(word.from, word.to);
+            new Notice(`Selected: ${editor.getSelection()}`);
+        } else {
+            new Notice('No word found under the cursor.');
+        }
+    }
+
+    // Function to delete the current selection
+    deleteSelection(editor: Editor) {
+        const selection = editor.getSelection();
+        if (selection) {
+            editor.replaceSelection('');
+            new Notice('Deleted selection.');
+        } else {
+            new Notice('No selection to delete.');
+        }
+    }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+// Settings tab for user configurations
+class HelixSettingTab extends PluginSettingTab {
+    plugin: HelixKeybindingPlugin;
 
-	async onload() {
-		await this.loadSettings();
+    constructor(app: App, plugin: HelixKeybindingPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Custom Setting')
+            .setDesc('Example setting description.')
+            .addText((text) =>
+                text
+                    .setPlaceholder('Enter a value')
+                    .setValue(this.plugin.settings.mySetting)
+                    .onChange(async (value) => {
+                        this.plugin.settings.mySetting = value;
+                        await this.plugin.saveSettings();
+                    })
+            );
+    }
 }
